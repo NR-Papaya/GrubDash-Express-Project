a54382684c43eb9cfcfb7feae873e147186a3231
Nahia Riviera <n.riviera.dev@gmail.com>
2022-05-31 11:58:41 -0500

    added functionality to dishes controller and orders controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,128 @@
+const { verify } = require("crypto");
 const path = require("path");
-
-// Use the existing dishes data
-const dishes = require(path.resolve("src/data/dishes-data"));
+const dishesData = require("../data/dishes-data");
+//Use the existing dishes data
+// const dishes = require(path.resolve("src/data/dishes-data"));
 
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+//------------Middleware Funcs
+const validateIdExists = (req, res, next) => {
+	const { dishId } = req.params;
+	const matchingDish = dishesData.find((dish) => dish.id === dishId);
+	if (matchingDish) {
+		res.locals.dish = matchingDish;
+		return next();
+	} else {
+		return next({
+			status: 404,
+			message: `A dish with an ID of: ${dishId} could not be found`,
+		});
+	}
+};
+
+const validateBodyId = (req, res, next) => {
+	const { data: { id } = {} } = req.body;
+	const { dishId } = req.params;
+	if (!id) {
+		next();
+	}
+	if (id !== dishId) {
+		next({
+			status: 400,
+			message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+		});
+	} else {
+		next();
+	}
+};
+
+const verifyHasProperty = (propertyName) => {
+	return function (req, res, next) {
+		const { data = {} } = req.body;
+		if (data[propertyName]) {
+			return next();
+		} else {
+			next({
+				status: 400,
+				message: `Dish must include a ${propertyName}`,
+			});
+		}
+	};
+};
+
+const validatePrice = (req, res, next) => {
+	const { data: { price } = {} } = req.body;
+	if (price <= 0 || Number.isInteger(price) === false) {
+		return next({
+			status: 400,
+			message: `Dish must have a price that is an integer greater than 0`,
+		});
+	} else {
+		next();
+	}
+};
+
+//------------CRUD Funcs
+
+const create = (req, res, next) => {
+	const {
+		data: { name, description, price, image_url },
+	} = req.body;
+	const newDish = {
+		name,
+		description,
+		price,
+		image_url,
+		id: nextId(),
+	};
+	dishesData.push(newDish);
+	res.status(201).json({ data: newDish });
+};
+
+const list = (req, res, next) => {
+	res.json({ data: dishesData });
+};
+
+const read = (req, res, next) => {
+	res.json({ data: res.locals.dish });
+};
+
+const update = (req, res, next) => {
+	const currentDish = res.locals.dish;
+	const {
+		data: { name, description, price, image_url },
+	} = req.body;
+	//update current dish with new data
+	currentDish.name = name;
+	currentDish.description = description;
+	currentDish.price = price;
+	currentDish["image_url"] = image_url;
+	res.json({ data: currentDish });
+};
+
+module.exports = {
+	create: [
+		verifyHasProperty("name"),
+		verifyHasProperty("description"),
+		verifyHasProperty("price"),
+		verifyHasProperty("image_url"),
+		validatePrice,
+		create,
+	],
+	list,
+	read: [validateIdExists, read],
+	update: [
+		verifyHasProperty("name"),
+		verifyHasProperty("description"),
+		verifyHasProperty("price"),
+		verifyHasProperty("image_url"),
+		validateIdExists,
+		validateBodyId,
+		validatePrice,
+		update,
+	],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,97 @@
 const path = require("path");
-
-// Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
 
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+//------------Middleware Funcs
+const validateId = (req, res, next) => {
+	const { orderId } = req.params;
+	const foundOrder = orders.find((order) => order.id === orderId);
+	if (foundOrder) {
+		res.locals.order = foundOrder;
+		next();
+	} else {
+		next({
+			status: 404,
+			message: `An order with an ID of ${orderId} could not be found.`,
+		});
+	}
+};
+const validateProperties = (property) => {
+	return function (req, res, next) {
+		const { data = {} } = req.body;
+		if (data[property]) {
+			return next();
+		} else {
+			next({
+				status: 400,
+				message: `Order must include a ${property}`,
+			});
+		}
+	};
+};
+const validateHasDishes = (req, res, next) => {
+	const { data: { dishes } = {} } = req.body;
+	if (Array.isArray(dishes) === false || dishes.length <= 0) {
+		next({ status: 400, message: `Order must include at least one dish` });
+	} else {
+		next();
+	}
+};
+const validateQuantityOfDish = (req, res, next) => {
+	const { data: { dishes } = {} } = req.body;
+	for (let i = 0; i < dishes.length; i++) {
+		if (
+			!dishes[i].quantity ||
+			dishes[i].quantity <= 0 ||
+			!Number.isInteger(dishes[i].quantity)
+		) {
+			return next({
+				status: 400,
+				message: `Dish ${i} must have a quantity that is an integer greater than 0`,
+			});
+		}
+	}
+	next();
+};
+
+//------------CRUD Funcs
+const create = (req, res, next) => {
+	const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+	const newOrder = {
+		id: nextId(),
+		deliverTo,
+		mobileNumber,
+		status: "delivered",
+		dishes,
+	};
+	orders.push(newOrder);
+	res.status(201).json({ data: newOrder });
+};
+
+const list = (req, res, next) => {
+	res.json({ data: orders });
+};
+const read = (req, res, next) => {
+	res.json({ data: res.locals.order });
+};
+
+const update = (req, res, next) => {};
+
+const destroy = (req, res, next) => {};
+
+module.exports = {
+	create: [
+		validateProperties("deliverTo"),
+		validateProperties("mobileNumber"),
+		validateProperties("dishes"),
+		validateHasDishes,
+		validateQuantityOfDish,
+		create,
+	],
+	list,
+	read: [validateId, read],
+	update,
+	destroy,
+};
